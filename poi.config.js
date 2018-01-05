72cedbeb7bc95340080271f10286b871c5f011ff
Protinam <protinam@protonmail.ch>
2018-01-04 18:17:01 -0800

    Update meta, contact, build.

--- poi.config.js
+++ poi.config.js
@@ -2,7 +2,7 @@ module.exports = (options, req) => ({
   entry: './src/index.js',
   html: {
     title: 'Project Wyvern',
-    description: 'Autonomouly governed decentralized digital item exchange',
+    description: 'Autonomouly governed decentralized digital item exchange running on the Ethereum blockchain.',
     template: 'src/index.ejs'
   }
 })
--- src/pages/Contact.vue
+++ src/pages/Contact.vue
@@ -2,7 +2,7 @@
 <div class="contact">
 The Wyvern Exchange protocol is permissionless – anyone can contribute – and the Exchange's governance is decentralized — run by <a target="_blank" href="https://dao.projectwyvern.com">the Wyvern DAO</a> — so if you want to build on the protocol or change the rules of the Exchange, you don't need to ask us first (and we can't stop you) — just <a target="_blank" href="https://github.com/projectwyvern">submit a pull request on Github</a> or <a target="_blank" href="https://dao.projectwyvern.com/proposals/create">create a proposal through the DAO web UI</a>.
 <br /><br />
-That said, we'd love to hear from you anyways! To contact the current development team, please email <a href="mailto:team@projectwyvern.com">team@projectwyvern.com</a>. For real-time support, jump on <a target="_blank" href="https://riot.im/app/#/room/#projectwyvern:matrix.org">our Matrix chat via Riot</a>.
+That said, we'd love to hear from you anyways! To contact the current development team, please email <a href="mailto:team@projectwyvern.com">team@projectwyvern.com</a>. For real-time support, jump on <a target="_blank" href="https://riot.im/app/#/room/#projectwyvern:matrix.org">our Matrix chat (via Riot)</a>.
 </div>
 </div>
 </template>
